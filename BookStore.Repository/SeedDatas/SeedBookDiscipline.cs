7bf09b2e3f5206817089055f7db3947ab86a4cb2
akdemirsafak <akdemirsafak@gmail.com>
2022-10-07 12:25:31 +0300

    SeedBookDiscipline

--- BookStore.Repository/SeedDatas/SeedBookDiscipline.cs
+++ BookStore.Repository/SeedDatas/SeedBookDiscipline.cs
@@ -9,16 +9,16 @@ public class SeedBookDiscipline : IEntityTypeConfiguration<BookDiscipline>
     public void Configure(EntityTypeBuilder<BookDiscipline> builder)
     {
         builder.HasData(
-            new BookDiscipline { BookId = 1, DisciplineId = 2 },
-            new BookDiscipline { BookId = 2, DisciplineId = 1 },
-            new BookDiscipline { BookId = 2, DisciplineId = 2 },
-            new BookDiscipline { BookId = 3, DisciplineId = 1 },
-            new BookDiscipline { BookId = 3, DisciplineId = 2 },
-            new BookDiscipline { BookId = 3, DisciplineId = 6 },
-            new BookDiscipline { BookId = 4, DisciplineId = 2 },
+            new BookDiscipline { BookId = 1, DisciplineId = 3 },
+            new BookDiscipline { BookId = 2, DisciplineId = 6 },
+            new BookDiscipline { BookId = 2, DisciplineId = 4 },
+            new BookDiscipline { BookId = 3, DisciplineId = 5 },
+            new BookDiscipline { BookId = 4, DisciplineId = 1 },
             new BookDiscipline { BookId = 5, DisciplineId = 1 },
             new BookDiscipline { BookId = 6, DisciplineId = 2 },
+            new BookDiscipline { BookId = 7, DisciplineId = 5 },
             new BookDiscipline { BookId = 7, DisciplineId = 2 },
+            new BookDiscipline { BookId = 8, DisciplineId = 2 },
             new BookDiscipline { BookId = 8, DisciplineId = 1 }
         );
     }
